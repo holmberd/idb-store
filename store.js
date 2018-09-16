17242726b7a8364e52f653379df8205a502a1d33
Dag Holmberg <holmberd@users.noreply.github.com>
2018-09-16 08:26:28 -0700

    Add query by index-name

--- store.js
+++ store.js
@@ -22,19 +22,26 @@ class Store {
    * Retrives a record or a range of records from the store.
    *
    * @param {Key|IDBKeyRange} key
+   * @param {String} [indexName]
    * @returns {Promise}
    */
-  get(key) {
+  get(key, indexName) {
     const connection = this.getConnection();
     return connection.then(db => {
       const tx = db.transaction(this.storeKey);
-      const val = tx.objectStore(this.storeKey).get(key);
+      var val = null;
+      if (indexName) {
+        val = tx.objectStore(this.storeKey).index(indexName).get(key);
+      } else {
+        val = tx.objectStore(this.storeKey).get(key);  
+      }
       db.close();
       return tx.complete.then(() => val);
     })
     .catch(err => {
-      err.message = 'Failed to get store record: ' + err.message;
-      return Promise.reject(err);
+      let _err = new Error(err);
+      _err.message = 'Failed to get store record: ' + _err.message;
+      return Promise.reject(_err);
     });
   }
 
@@ -47,16 +54,16 @@ class Store {
    */
   set(val, key) {
     const connection = this.getConnection();
-    let _db = null;
     return connection.then(db => {
-      const tx = db.transaction(this.storeKey, 'readwrite');
-      tx.objectStore(this.storeKey).put(val, key);
+      var tx = db.transaction(this.storeKey, 'readwrite');
+      var request = tx.objectStore(this.storeKey).put(val, key);
       db.close();
-      return tx.complete;
+      return Promise.all([request, tx.complete]);
     })
     .catch(err => {
-      err.message = 'Failed to set store record: ' + err.message;
-      return Promise.reject(err);
+      let _err = new Error(err);
+      _err.message = 'Failed to set store record: ' + _err.message;
+      return Promise.reject(_err);
     });
   }
 
@@ -64,19 +71,25 @@ class Store {
    * Removes a key or a range of keys from the store.
    *
    * @param {Key|IDBKeyRange} key
+   * @param {String} [indexName]
    * @returns {Promise}
    */
-  delete(key) {
+  delete(key, indexName) {
     const connection = this.getConnection();
     return connection.then(db => {
       const tx = db.transaction(this.storeKey, 'readwrite');
-      tx.objectStore(this.storeKey).delete(key);
+      if (indexName) {
+        tx.objectStore(this.storeKey).index(indexName).delete(key);  
+      } else {
+        tx.objectStore(this.storeKey).delete(key);
+      }
       db.close();
       return tx.complete;
     })
     .catch(err => {
-      err.message = 'Failed to remove store record: ' + err.message;
-      return Promise.reject(err);
+      let _err = new Error(err);
+      _err.message = 'Failed to remove store record: ' + _err.message;
+      return Promise.reject(_err);
     });
   }
 
@@ -94,8 +107,9 @@ class Store {
       return tx.complete;
     })
     .catch(err => {
-      err.message = 'Failed to clear store records: ' + err.message;
-      return Promise.reject(err);
+      let _err = new Error(err);
+      _err.message = 'Failed to clear store records: ' + _err.message;
+      return Promise.reject(_err);
     });
   }
 
@@ -122,8 +136,9 @@ class Store {
       return tx.complete.then(() => keys);
     })
     .catch(err => {
-      err.message = 'Failed to get store keys: ' + err.message;
-      return Promise.reject(err);
+      let _err = new Error(err);
+      _err.message = 'Failed to get store keys: ' + _err.message;
+      return Promise.reject(_err);
     });
   }
 
@@ -134,20 +149,25 @@ class Store {
    *
    * @param {Key|IDBKeyRange} [query]
    * @param {Number} [count] - number of values to return
+   * @param {String} [indexName]
    * @returns {Promise}
    */
-  getAll(query, count) {
+  getAll(query, count, indexName) {
     const connection = this.getConnection();
     return connection.then(db => {
       const tx = db.transaction(this.storeKey);
-      const store = tx.objectStore(this.storeKey);
+      var store = tx.objectStore(this.storeKey);
+      if (indexName) {
+        store = tx.objectStore(this.storeKey).index(indexName);
+      }
       const keys = store.getAll(query, count);
       db.close();
       return tx.complete.then(() => keys);
     })
     .catch(err => {
-      err.message = 'Failed to get all store records: ' + err.message;
-      return Promise.reject(err);
+      let _err = new Error(err);
+      _err.message = 'Failed to get all store records: ' + _err.message;
+      return Promise.reject(_err);
     });
   }
 
@@ -156,19 +176,26 @@ class Store {
    * that match the provided key or IDBKeyRange.
    *
    * @param {Key|IDBKeyrange} [query]
+   * @param {String} [indexName]
    * @returns {Promise}
    */
-  count(query) {
+  count(query, indexName) {
     const connection = this.getConnection();
     return connection.then(db => {
       const tx = db.transaction(this.storeKey);
-      const val = tx.objectStore(this.storeKey).count(query);
+      var val = null;
+      if (indexName) {
+        val = tx.objectStore(this.storeKey).index(indexName).count(query);
+      } else {
+        val = tx.objectStore(this.storeKey).count(query);  
+      }
       db.close();
       return tx.complete.then(() => val);
     })
     .catch(err => {
-      err.message = 'Failed to get store count: ' + err.message;
-      return Promise.reject(err);
+      let _err = new Error(err);
+      _err.message = 'Failed to get store count: ' + _err.message;
+      return Promise.reject(_err);
     });
   }
 
